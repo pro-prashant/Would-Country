aacea679a70a23291fc8584383545a886db33325
prashantyr <125874396+pro-prashant@users.noreply.github.com>
2024-11-23 15:10:00 +0530

    Update Footer.jsx

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import footerContact from "../../api/footerApi.json";
+import footerContact from "../../api/FooterApi.json";
 import { IoCallSharp } from "react-icons/io5";
 import { MdPlace } from "react-icons/md";
 import { TbMailPlus } from "react-icons/tb";
